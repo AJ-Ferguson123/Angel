e038e74092756d9989e72d1df10f119f703dd03d
Angel-TheDev <angelantoinetted@yahoo.com>
2019-07-26 11:40:45 -0400

    Added Manu Controller, Models, and empty SiteContext

---
+++ Angel/Model/Collection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Angel.Model
+{
+    public class Collection
+    {
+        public int CollectionId { get; set; }
+        public int ManuId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
---
+++ Angel/Model/Manufacturer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Angel.Model
+{
+    public class Manufacturer
+    {
+        public int ManuId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageURL { get; set; }
+        public string Country { get; set; }
+
+
+
+    }
+}
