056e9886ce025cfb9db15ee116d234aaf02c3bdc
AJFerguson123 <roverrecovery1@gmail.com>
2019-07-26 16:17:07 -0400

    edit collection js with a comment out section

---
+++ parcel-frontend/js/components/collection.js
@@ -0,0 +1,14 @@
+export default function Collection(){
+    return`
+    <ul>
+    <li
+    id="collection_diver">Diver</li>
+    <li
+    id="collection_pilot">Pilot</li>
+    <li
+    id="collection_dress">Dress</li>
+    <li
+    id="collection_chronograph">Chronograph</li>
+    </ul>
+    `
+}
