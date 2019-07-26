3761f7cb634f6d4c8c3bef5d3c2648d6e33ad2f3
AJFerguson123 <roverrecovery1@gmail.com>
2019-07-26 13:04:50 -0400

    edited controller

--- Angel/Controllers/ManufacturerController.cs
+++ Angel/Controllers/ManufacturerController.cs
@@ -15,14 +15,14 @@ namespace Angel.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Rolex" };
         }
 
         // GET api/<controller>/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return "manufacturer";
         }
 
         // POST api/<controller>
